a0bca7470ef47e3a26ace64536d2985a3bf381d2
catsriel <danbro07@imkatta.no>
2025-04-30 11:10:19 +0200

    Added basic content

---
+++ calendar/src/navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar(){
+    return(
+        <nav>
+            <img src="public\catsrieldreemurr.png" height={120}></img>
+        </nav>
+    )
+}
+
+export default Navbar;
